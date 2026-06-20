0eb4d0d0f34f9d09be63b6d6fee0980e14cb58e0
sahin <sahindafader1919@gmail.com>
2026-06-20 20:34:27 +0530

    modified user entity

--- src/module/users/entities/user.entity.ts
+++ src/module/users/entities/user.entity.ts
@@ -1,52 +1,60 @@
-import { AuthProvider, UserRole, UserStatus } from "src/config/enum";
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, OneToMany, OneToOne } from "typeorm";
-import { UserOrganization } from "./userorganization.entity";
-import { Auth } from "src/module/auth/entities/auth.entity";
+import { AuthProvider, UserRole, UserStatus } from 'src/config/enum';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+  OneToOne,
+} from 'typeorm';
+import { UserOrganization } from './userorganization.entity';
+import { Auth } from 'src/module/auth/entities/auth.entity';
 
-@Entity("users")
+@Entity('users')
 export class User {
-    @PrimaryGeneratedColumn("uuid") // UUID primary key
-    user_id: string;
+  @PrimaryGeneratedColumn('uuid') // UUID primary key
+  user_id: string;
 
-    @Column({ length: 100 })
-    firstname: string;
+  @Column({ length: 100 })
+  firstname: string;
 
-    @Column({ length: 100 })
-    lastname: string;
+  @Column({ length: 100 })
+  lastname: string;
 
-    @Column({ length: 100, unique: true })
-    email: string;
+  @Column({ length: 100, unique: true })
+  email: string;
 
-    @Column({ type:"enum", enum:AuthProvider})
-    oauth_provider?:AuthProvider;
+  @Column({ type: 'enum', enum: AuthProvider })
+  oauth_provider?: AuthProvider;
 
-    @Column({ nullable: true })
-    oauth_id?: string;
+  @Column({ nullable: true })
+  oauth_id?: string;
 
-    @Column({ default: false })
-    email_verified?: boolean;
+  @Column({ default: false })
+  email_verified?: boolean;
 
-    @Column({ type: "enum", enum: UserRole })
-    role: UserRole;
+  @Column({ type: 'enum', enum: UserRole })
+  role: UserRole;
 
-    @Column({ type: "smallint", default: UserStatus.ACTIVE })
-    status: UserStatus;
+  @Column({ type: 'smallint', default: UserStatus.ACTIVE })
+  status: UserStatus;
 
-    @Column({ length: 500, nullable: true })
-    profile_pic?: string | null;
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  profile_pic?: string | null;
 
-    @Column({ type: "boolean", default: false })
-    is_deleted: boolean;
+  @Column({ type: 'boolean', default: false })
+  is_deleted: boolean;
 
-    @CreateDateColumn({ type: "timestamptz" })
-    created_at: Date;
+  @CreateDateColumn({ type: 'timestamptz' })
+  created_at: Date;
 
-    @UpdateDateColumn({ type: "timestamptz", nullable: true })
-    updated_at?: Date;
+  @UpdateDateColumn({ type: 'timestamptz', nullable: true })
+  updated_at?: Date;
 
-    @OneToMany(() => UserOrganization, (userOrg) => userOrg.user)
-    userOrganizations: UserOrganization[];
+  @OneToMany(() => UserOrganization, (userOrg) => userOrg.user)
+  userOrganizations: UserOrganization[];
 
-    @OneToOne(() => Auth, (auth) => auth.user) 
-      auth: Auth;                                
+  @OneToOne(() => Auth, (auth) => auth.user)
+  auth: Auth;
 }
