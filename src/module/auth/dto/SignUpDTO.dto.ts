db1a9181fa89665291f36942d036b2de6b72d15a
sahin <sahindafader1919@gmail.com>
2026-06-28 19:16:33 +0530

    signup dto modify

--- src/module/auth/dto/SignUpDTO.dto.ts
+++ src/module/auth/dto/SignUpDTO.dto.ts
@@ -1,43 +1,64 @@
-import { IsBoolean, IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
-import { AuthProvider, UserRole, UserStatus } from "src/config/enum";
+import {
+  IsBoolean,
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Length,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { AuthProvider, UserRole, UserStatus } from 'src/config/enum';
 
 export class InputData {
-    @IsString()
-    @IsNotEmpty()
-    firstname: string; 
+  @IsString()
+  @IsNotEmpty({ message: 'First name cannot be empty or whitespace.' })
+  @Length(1, 100, {
+    message: 'First name must be between 1 and 100 characters.',
+  })
+  @Matches(/^[a-zA-Z\s\-'\u00C0-\u017F]+$/, {
+    message: 'First name contains invalid characters.',
+  })
+  firstname: string;
 
-    @IsString()
-    @IsNotEmpty()
-    lastname:string;
+  @IsString()
+  @IsOptional()
+  @Length(1, 100, {
+    message: 'Last name must be between 1 and 100 characters.',
+  })
+  @Matches(/^[a-zA-Z\s\-'\u00C0-\u017F]+$/, {
+    message: 'Last name contains invalid characters.',
+  })
+  lastname?: string;
 
-    @IsEmail() // Specifically validates the email format (e.g., name@domain.com)
-    @IsNotEmpty()
-    email: string;
+  @IsEmail() // Specifically validates the email format (e.g., name@domain.com)
+  @IsNotEmpty()
+  email: string;
 }
-export class UserDataDto extends InputData{
+export class UserDataDto extends InputData {
+  @IsEnum(UserRole)
+  @IsOptional()
+  role?: UserRole;
 
-    @IsEnum(UserRole)
-    @IsOptional() 
-    role?: UserRole;
+  @IsEnum(UserStatus)
+  @IsOptional()
+  status?: UserStatus;
 
-    @IsEnum(UserStatus)
-    @IsOptional() 
-    status?:UserStatus
+  @IsBoolean()
+  @IsOptional()
+  is_deleted?: boolean;
 
-    @IsBoolean()
-    @IsOptional()
-    is_deleted?:boolean;
-
-    @IsEnum(AuthProvider)
-    @IsOptional() 
-    oauth_provider?:AuthProvider;
+  @IsEnum(AuthProvider)
+  @IsOptional()
+  oauth_provider?: AuthProvider;
 }
 
 export class SignUpDto extends InputData {
-
-    @IsString()
-    @IsNotEmpty()
-    @MinLength(8, { message: 'Password is too short (min 8 characters)' })
-    @MaxLength(32, { message: 'New password cannot exceed 32 characters' })
-    password: string;
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8, { message: 'Password is too short (min 8 characters)' })
+  @MaxLength(32, { message: 'New password cannot exceed 32 characters' })
+  password: string;
 }
