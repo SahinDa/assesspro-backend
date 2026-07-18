37cb7a66bb754b70ef7bfa144dae3add47ac6cb6
sahin <sahindafader1919@gmail.com>
2026-07-18 15:32:32 +0530

    feat: configure Redis connection using ioredis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService  } from '@nestjs/config'; // Import this
-import { JwtModule } from "@nestjs/jwt";
+import { ConfigModule, ConfigService } from '@nestjs/config'; // Import this
+import { JwtModule } from '@nestjs/jwt';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AttemptsModule } from './module/attempts/attempts.module';
@@ -17,9 +17,7 @@ import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { ResponseInterceptor } from './interceptors/responseinterceptor';
 import { AuthGuard } from './guards/authentication.guard';
 import { RoleGuard } from './guards/role.guard';
-
-
-
+import { RedisModule } from '@nestjs-modules/ioredis';
 @Module({
   imports: [
     // 1. Load the .env file globally
@@ -38,16 +36,28 @@ import { RoleGuard } from './guards/role.guard';
       },
     }),
 
-     JwtModule.registerAsync({
-          imports: [ConfigModule],
-          inject: [ConfigService],
-          global: true, 
-          useFactory: async (configService: ConfigService) => ({
-            secret: configService.get<string>('JWT_SECRETKEY'), 
-            signOptions: { expiresIn: '1d' },
-          }),
-        }),
-  
+    // 3. Setup the Global Redis Cache Connection Pool (Upstash)
+    RedisModule.forRootAsync({
+      useFactory: () => ({
+        type: 'single',
+        url: process.env.REDIS_URL,
+        isGlobal: true, // 🌟 Makes the Redis instance available everywhere in your app
+        options: {
+          tls: {}, // Required for secure Upstash connection strings
+          connectTimeout: 10000,
+        },
+      }),
+    }),
+
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      global: true,
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRETKEY'),
+        signOptions: { expiresIn: '1d' },
+      }),
+    }),
 
     // 3. FEATURE MODULES (The "Domain")
     AttemptsModule,
@@ -59,20 +69,19 @@ import { RoleGuard } from './guards/role.guard';
     SubscriptionModule,
     TestModule,
     UserModule,
-    
   ],
   controllers: [AppController],
   providers: [
     AppService,
-      {
-        provide: APP_GUARD,
-        useClass:AuthGuard
-      },
-      {
-        provide: APP_GUARD,
-        useClass:RoleGuard,
-      },
-      {
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard,
+    },
+    {
       provide: APP_INTERCEPTOR,
       useClass: ResponseInterceptor,
     },
