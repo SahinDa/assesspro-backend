d38f9b2de551d2542b89c7c6276887e6218f1f1a
sahin <sahindafader1919@gmail.com>
2026-08-23 11:31:06 +0530

    chore: configure dynamic port and 0.0.0.0 host binding for deployment

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './filters/allexceptions.filter';
 import { ValidationPipe } from '@nestjs/common';
-import  cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter());
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.setGlobalPrefix('/api')
+  app.setGlobalPrefix('/api');
   app.use(cookieParser());
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
