12236e1b4a14fe7eb20678039b4f3ee3f24bed84
Sahin Dafader <90916625+SahinDa@users.noreply.github.com>
2026-09-08 23:25:04 +0530

    Update logoutinterceptor.ts

--- src/interceptors/logoutinterceptor.ts
+++ src/interceptors/logoutinterceptor.ts
@@ -2,23 +2,21 @@ import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nes
 import { map, Observable } from "rxjs";
 import { Response } from "express";
 
+const COOKIE_OPTIONS = {
+  httpOnly: true,
+  secure: true,
+  sameSite: 'none' as const,
+  path: '/',
+};
+
 @Injectable()
 export class LogoutInterceptor implements NestInterceptor {
     intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> {
         const response = context.switchToHttp().getResponse<Response>();
         return next.handle().pipe(
             map((payload) => {
-
-                response.clearCookie('accessToken', {
-                    httpOnly: true,
-                    secure: true,
-                    sameSite: 'lax',
-                });
-                response.clearCookie('refreshToken', {
-                    httpOnly: true,
-                    secure: true,
-                    sameSite: 'lax',
-                });
+        response.clearCookie('accessToken', COOKIE_OPTIONS);
+        response.clearCookie('refreshToken', COOKIE_OPTIONS);
 
                 return payload;
             })
