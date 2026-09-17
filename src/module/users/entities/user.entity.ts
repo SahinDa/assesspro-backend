a1d60cd6eee233906b587faec3e5fe6c2f4a408b
Sahin Dafader <90916625+SahinDa@users.noreply.github.com>
2026-09-17 23:39:38 +0530

    Update user.entity.ts

--- src/module/users/entities/user.entity.ts
+++ src/module/users/entities/user.entity.ts
@@ -10,6 +10,7 @@ import {
 } from 'typeorm';
 import { UserOrganization } from './userorganization.entity';
 import { Auth } from 'src/module/auth/entities/auth.entity';
+import { Organization } from 'src/module/organizations/entities/organization.entity';
 
 @Entity('users')
 export class User {
@@ -54,6 +55,10 @@ export class User {
   
  @Column({ type: 'uuid', nullable: true })
   active_org_id?: string | null;
+
+  @ManyToOne(() => Organization, { nullable: true, onDelete: 'SET NULL' })
+  @JoinColumn({ name: 'active_org_id' })
+  activeOrganization?: Organization | null;
   
   @OneToMany(() => UserOrganization, (userOrg) => userOrg.user)
   userOrganizations: UserOrganization[];
