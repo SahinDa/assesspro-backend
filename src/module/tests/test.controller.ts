fcc44f8608dc2b22287066f02056d8551c1da283
Sahin Dafader <90916625+SahinDa@users.noreply.github.com>
2026-09-22 22:11:57 +0530

    Update test.controller.ts

--- src/module/tests/test.controller.ts
+++ src/module/tests/test.controller.ts
@@ -65,11 +65,11 @@ export class TestController {
   @Roles(UserRole.ADMIN, UserRole.ORGANIZATION, UserRole.STUDENT)
   async getAllTestList(
     @Organization() organization: IOrganization,
+    @Query('offset', new DefaultValuePipe(0), ParseIntPipe) offset: number = 0,
+    @Query('limit', new DefaultValuePipe(12), ParseIntPipe) limit: number = 12,
     @Query('id', new ParseUUIDPipe({ version: '4', optional: true }))
     id?: string,
     @Query('status', new ParseIntPipe({ optional: true })) status?: number,
-    @Query('offset', new DefaultValuePipe(0), ParseIntPipe) offset: number = 0,
-    @Query('limit', new DefaultValuePipe(12), ParseIntPipe) limit: number = 12,
   ) {
     return await this.testService.getAllTestList(organization, id, status,offset,limit);
   }
